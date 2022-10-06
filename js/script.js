349daf9584f9c7ac91738865ed85309d77e46659
Adrian Amaral <adri13amaral@gmail.com>
2022-10-06 09:50:50 -0300

    calculo de distancias onde os eventos ocorrem

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-import Slide from "./slide.js";
+import Slide from './slide.js';
 
-const slide = new Slide('.lide', '.slide-wrapper');
+const slide = new Slide('.slide', '.slide-wrapper');
+slide.init();
+console.log(slide);
